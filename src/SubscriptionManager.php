958ac2c4abed30853af6ca77095a171fc0bd40e2
bishoynabil224 <beshoynabil224@gmail.com>
2026-05-11 16:04:30 +0300

    Update SubscriptionManager.php

--- src/SubscriptionManager.php
+++ src/SubscriptionManager.php
@@ -3,7 +3,7 @@ namespace App;
 
 class SubscriptionManager {
     public function getDaysRemaining($totalDays, $daysUsed) {
-        // احسب الفرق مباشرة
+        
         $remaining = $totalDays - $daysUsed;
         
         return ($remaining > 0) ? $remaining : 0;
